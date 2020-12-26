2f51c8e95e0c9e855704aabbeb1606fcc9b3bd63
NoahAndrews <noah@revrobotics.com>
2020-12-26 16:18:17 -0600

    initial commit

---
+++ app/src/main/java/com/example/remoexternalcontroldemo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.remoexternalcontroldemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.remoexternalcontroldemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val apiKey = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener() {
+            val intent = Intent("tv.remo.android.controller.action.REQUEST_REMO_STREAM_START")
+            intent.putExtra("apiKey", apiKey)
+
+            startActivityForResult(intent, 0)
+        }
+    }
+}
